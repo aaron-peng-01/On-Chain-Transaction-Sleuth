a9c26d8d4a2f72f14aada22e9af69998576076b8
aaron.peng <87630119+aaron-peng-01@users.noreply.github.com>
2025-10-05 13:44:28 +0800

    Update main.go

--- main.go
+++ main.go
@@ -10,20 +10,17 @@ import (
 )
 
 const (
-	// 强烈建议从环境变量读取，这里为了演示方便直接写入
 	// e.g., "https://eth-mainnet.g.alchemy.com/v2/YOUR_ALCHEMY_KEY"
 	nodeUrl = "YOUR_ETHEREUM_NODE_RPC_URL"
 )
 
 func main() {
-	// rpc.Dial会建立原始的RPC连接，我们需要它来调用debug模块
+	// rpc.Dial建立原始的RPC连接，使其调用debug模块
 	rpcClient, err := rpc.Dial(nodeUrl)
 	if err != nil {
 		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
 	}
 
-	// ethclient.NewClient是基于RPC连接的封装，方便调用标准接口
-	// 虽然我们主要用rpcClient，但保留它也很方便
 	ethClient := ethclient.NewClient(rpcClient)
 	_ = ethClient // 避免 "unused variable" 错误
 
